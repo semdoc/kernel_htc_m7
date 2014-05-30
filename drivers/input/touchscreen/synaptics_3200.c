36b605933eaad9452fdc72bfce5f3e01136d30b0
flar2 <asegaert@gmail.com>
2014-05-30 22:33:06 +0300

    dt2w: enable only on the bottom part of screen
    Conflicts:
    	drivers/input/touchscreen/synaptics_3200.c
    
    Change-Id: I0845529ef2626107e5b12fbdc1f87d1fe8eec827
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/input/touchscreen/synaptics_3200.c
+++ drivers/input/touchscreen/synaptics_3200.c
@@ -2450,8 +2450,8 @@ static void dt2w_func(int x, int y, cputime64_t trigger_time) {
 
         if (scr_suspended) {
 		if (
-		y < 2880
-		&& y > 2000
+		y < 2880 && y > 2100
+		&& x > 150 && x < 1470
 		&& ((dt2w_time[0]-dt2w_time[1]) > DT2W_TIMEOUT_MIN)
 		&& ((dt2w_time[0]-dt2w_time[1]) < DT2W_TIMEOUT_MAX)
 		&& (abs(delta_x) < DT2W_DELTA)
